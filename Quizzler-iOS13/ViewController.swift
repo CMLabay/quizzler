a8bfd76f23e78cd9368be35f6bd9ef77ecace34b
Christina Labay <Christina.m.labay@gmail.com>
2020-04-26 14:35:31 -0500

    add question struct, button actions

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
     
+    
+    let quiz = [
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+    ]
+    var questionNum = 0
+//    var myQuiz = quiz()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
+        
     }
 
-
+    @IBAction func AnswerBtnPressed(_ sender: UIButton) {
+        
+        let userAns = sender.currentTitle
+        let actualAns = quiz[questionNum].answer
+        if userAns == actualAns {
+            print("correct")
+        } else {
+            print("wrong")
+        }
+        if questionNum < quiz.count - 1 {
+            questionNum += 1
+        } else {
+            questionNum = 0
+        }
+        
+        updateUI()
+    }
+    func updateUI(){
+        questionLabel.text = quiz[questionNum].text
+    }
 }
 
